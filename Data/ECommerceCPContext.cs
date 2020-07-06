bb328727d5da5f755257c60360a5a550d01c6044
guillaume cadieux <guillaume.cadieux@lepisc.com>
2020-07-06 12:41:15 -0400

    Add project files.

---
+++ Data/ECommerceCPContext.cs
@@ -0,0 +1,26 @@
+﻿using e_commerce_cp.EntityConfigurations;
+using e_commerce_cp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_cp.Data
+{
+    public class ECommerceCPContext : DbContext
+    {
+        public ECommerceCPContext(DbContextOptions<ECommerceCPContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Dummy> Dummies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new DummyConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
